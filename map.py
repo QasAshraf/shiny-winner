1f934537eb516ed147b7979c93aac9de31d3325a
Qas Ashraf <mail@qasashraf.co.uk>
2017-10-28 23:10:41 +0100

    Reduce chance of obstacles overlapping with tank spawn points

--- map.py
+++ map.py
@@ -3,17 +3,21 @@ import random
 import pygame
 from obstacle import Obstacle
 
+SIZE_OF_TANK = 78
+NUMBER_OF_OBSTACLES = 5
+
 class Map:
+
+
     def __init__(self, screenWidth, screenHeight):
-        self.width = screenWidth
-        self.height = screenHeight
-        self.numberOfObstacles = 5
+        self.width = screenWidth - SIZE_OF_TANK
+        self.height = screenHeight - SIZE_OF_TANK
 
     def createObstacles(self):
         self.obstacles = []
-        for i in range(0, self.numberOfObstacles):
-            x = random.randint(0, self.width)
-            y = random.randint(0, self.height)
+        for i in range(0, NUMBER_OF_OBSTACLES):
+            x = random.randint(SIZE_OF_TANK, self.width)
+            y = random.randint(SIZE_OF_TANK, self.height)
             print("Creating obstacle at: " + str(x) + ", " + str(y))
             self.obstacles.append(Obstacle(x, y))
         return self.obstacles
