9251b842c661d259a674427f09fee23003bc39a7
Qas Ashraf <mail@qasashraf.co.uk>
2017-10-28 16:56:01 +0100

    Debug joysticks and print out things

--- game.py
+++ game.py
@@ -8,7 +8,38 @@ BLACK    = (   0,   0,   0)
 WHITE    = ( 255, 255, 255)
 
 # Start game
-started = pygame.init()
+pygame.init()
+pygame.joystick.init()
+
+def printJoystickInfo(joystickCount):
+    # For each joystick:
+    for i in range(joystickCount):
+        joystick = pygame.joystick.Joystick(i)
+        joystick.init()
+
+        print("Joystick {}".format(i))
+
+        # Get the name from the OS for the controller/joystick
+        name = joystick.get_name()
+        print("Joystick name {}".format(name))
+
+        # Usually axis run in pairs, up/down for one, and left/right for
+        # the other.
+        axes = joystick.get_numaxes()
+        print("Number of axes: {}".format(axes))
+
+        buttons = joystick.get_numbuttons()
+        print("Number of buttons: {}".format(buttons))
+
+        # Hat switch. All or nothing for direction, not like joysticks.
+        # Value comes back in an array.
+        hats = joystick.get_numhats()
+        print("Number of hats: {}".format(hats))
+
+# List out joysticks and their capabilities
+joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
+print("Joystick count: " + str(pygame.joystick.get_count()))
+printJoystickInfo(pygame.joystick.get_count())
 
 # Screen settings
 screenWidth = 1024
@@ -22,7 +53,11 @@ screen.fill(WHITE)
 
 def eventHandler():
     for event in pygame.event.get():
-        print(event)
+        #print(event)
+        # Possible joystick actions: JOYAXISMOTION JOYBALLMOTION JOYBUTTONDOWN JOYBUTTONUP JOYHATMOTION
+        if event.type == pygame.JOYBUTTONDOWN or event.type == pygame.JOYBUTTONUP or event.type == pygame.JOYAXISMOTION or event.type == pygame.JOYBALLMOTION or event.type == pygame.JOYHATMOTION:
+            print(">>>>>>>> Joystick event")
+            print(str(event.type))
         if event.type == QUIT:
             pygame.quit()
             quit()
