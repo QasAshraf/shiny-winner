7b978df0bf1a2369253bb357121a9962d635c96f
Steph Cook <3418146+stephegn@users.noreply.github.com>
2017-10-28 18:31:06 +0100

    Split all joystick interaction into a new class

--- game.py
+++ game.py
@@ -3,8 +3,7 @@
 import pygame
 from pygame.locals import *
 from tank import Tank
-
-import xbox360_controller
+from joystick import Joysticks
 
 # colours
 BLACK    = (   0,   0,   0)
@@ -18,42 +17,6 @@ PURPLE = (255, 0, 255)
 pygame.init()
 pygame.joystick.init()
 
-def printJoystickInfo(joystickCount):
-    # For each joystick:
-    for i in range(joystickCount):
-        joystick = pygame.joystick.Joystick(i)
-        joystick.init()
-
-        print("\tJoystick {}".format(i))
-
-        # Get the name from the OS for the controller/joystick
-        name = joystick.get_name()
-        print("\tJoystick name {}".format(name))
-
-        # Usually axis run in pairs, up/down for one, and left/right for
-        # the other.
-        axes = joystick.get_numaxes()
-        print("\t\tNumber of axes: {}".format(axes))
-
-        buttons = joystick.get_numbuttons()
-        print("\t\tNumber of buttons: {}".format(buttons))
-
-        # Hat switch. All or nothing for direction, not like joysticks.
-        # Value comes back in an array.
-        hats = joystick.get_numhats()
-        print("\t\tNumber of hats: {}".format(hats))
-
-# List out joysticks and their details
-numberOfJoysticks = pygame.joystick.get_count()
-joysticks = [pygame.joystick.Joystick(x) for x in range(numberOfJoysticks)]
-print("Joystick count: " + str(numberOfJoysticks))
-printJoystickInfo(numberOfJoysticks)
-
-# Create a controller object per controller
-# TOOD: Support multiple controllers
-if numberOfJoysticks > 0:
-    controller = xbox360_controller.Controller(0)
-
 # Screen settings
 screenWidth = 1024
 screenHeight = 768
@@ -65,6 +28,7 @@ printAllEvents = False
 # Show screen
 screen = pygame.display.set_mode((screenWidth, screenHeight))
 pygame.display.set_caption(screenTitle)
+joysticks = Joysticks()
 
 allSpritesList = pygame.sprite.Group()
 
@@ -78,50 +42,17 @@ tank = createTank()
 allSpritesList.add(tank)
 
 
-# Do stuff when buttons are pressed
-def buttonAPress(controllerId):
-    print("Controller {} pressed A".format(controllerId))
-
-def buttonBPress(controllerId):
-    print("Controller {} pressed B".format(controllerId))
-
-def buttonXPress(controllerId):
-    print("Controller {} pressed X".format(controllerId))
-
-def buttonYPress(controllerId):
-    print("Controller {} pressed Y".format(controllerId))
-
-def buttonYPress(controllerId):
-    print("Controller {} pressed Y".format(controllerId))
-
-def joystickButtonHandler(controllerId, event):
-    # Handle events for first controller
-    if event.type == pygame.JOYBUTTONDOWN:
-        if event.joy == controllerId:
-            if event.button == xbox360_controller.A:
-                buttonAPress(controllerId)
-            elif event.button == xbox360_controller.B:
-                buttonBPress(controllerId)
-            elif event.button == xbox360_controller.X:
-                buttonXPress(controllerId)
-            elif event.button == xbox360_controller.Y:
-                buttonYPress(controllerId)
-
-def joystickPadHandler(xboxController):
-    pad_up, pad_right, pad_down, pad_left = xboxController.get_pad()
-    padPressed = pad_up + pad_right + pad_left + pad_down
+def joystickPadHandler(pad_up, pad_right, pad_down, pad_left):
     MOVEMENT_MULTIPLIER = 3
-    if padPressed > 0:
-        print("Controller: {} --> Up: {}, Down: {}, Left: {}, Right: {}".format(xboxController.get_id(), pad_up, pad_down, pad_left, pad_right))
-        tank.move(pad_up * MOVEMENT_MULTIPLIER, pad_down * MOVEMENT_MULTIPLIER, pad_left * MOVEMENT_MULTIPLIER, pad_right * MOVEMENT_MULTIPLIER)
+    tank.move(pad_up * MOVEMENT_MULTIPLIER, pad_down * MOVEMENT_MULTIPLIER, pad_left * MOVEMENT_MULTIPLIER,
+                  pad_right * MOVEMENT_MULTIPLIER)
+
 
 def eventHandler():
     for event in pygame.event.get():
-        if numberOfJoysticks > 0:
-            controllerId = controller.get_id()
-            joystickButtonHandler(controllerId, event)
-            # TODO: Handle pad for multiple joysticks
-            joystickPadHandler(controller)
+        if joysticks.hasJoysticks():
+            joysticks.joystickButtonHandler(event)
+            joysticks.joystickPadHandler(joystickPadHandler)
 
         # Handle quit of game or any other events
         if event.type == QUIT:
---
+++ joystick.py
@@ -0,0 +1,78 @@
+import pygame
+import xbox360_controller
+
+class Joysticks:
+    def __init__(self):
+        # List out joysticks and their details
+        self.numberOfJoysticks = pygame.joystick.get_count()
+        joysticks = [pygame.joystick.Joystick(x) for x in range(self.numberOfJoysticks)]
+        print("Joystick count: " + str(self.numberOfJoysticks))
+        self.printJoystickInfo()
+
+        # Create a controller object per controller
+        # TOOD: Support multiple controllers
+        if self.numberOfJoysticks > 0:
+            self.controller = xbox360_controller.Controller(0)
+
+    def hasJoysticks(self):
+        return self.numberOfJoysticks > 0
+    def printJoystickInfo(self):
+        # For each joystick:
+        for i in range(self.numberOfJoysticks):
+            joystick = pygame.joystick.Joystick(i)
+            joystick.init()
+
+            print("\tJoystick {}".format(i))
+
+            # Get the name from the OS for the controller/joystick
+            name = joystick.get_name()
+            print("\tJoystick name {}".format(name))
+
+            # Usually axis run in pairs, up/down for one, and left/right for
+            # the other.
+            axes = joystick.get_numaxes()
+            print("\t\tNumber of axes: {}".format(axes))
+
+            buttons = joystick.get_numbuttons()
+            print("\t\tNumber of buttons: {}".format(buttons))
+
+            # Hat switch. All or nothing for direction, not like joysticks.
+            # Value comes back in an array.
+            hats = joystick.get_numhats()
+            print("\t\tNumber of hats: {}".format(hats))
+
+    # Do stuff when buttons are pressed
+    def buttonAPress(self, controllerId):
+        print("Controller {} pressed A".format(controllerId))
+
+    def buttonBPress(self, controllerId):
+        print("Controller {} pressed B".format(controllerId))
+
+    def buttonXPress(self, controllerId):
+        print("Controller {} pressed X".format(controllerId))
+
+    def buttonYPress(self, controllerId):
+        print("Controller {} pressed Y".format(controllerId))
+
+    def joystickButtonHandler(self, event):
+        controllerId = self.controller.get_id()
+        # Handle events for first controller
+        if event.type == pygame.JOYBUTTONDOWN:
+            if event.joy == controllerId:
+                if event.button == xbox360_controller.A:
+                    self.buttonAPress(controllerId)
+                elif event.button == xbox360_controller.B:
+                    self.buttonBPress(controllerId)
+                elif event.button == xbox360_controller.X:
+                    self.buttonXPress(controllerId)
+                elif event.button == xbox360_controller.Y:
+                    self.buttonYPress(controllerId)
+
+    def joystickPadHandler(self, callback):
+        pad_up, pad_right, pad_down, pad_left = self.controller.get_pad()
+        padPressed = pad_up + pad_right + pad_left + pad_down
+        if padPressed > 0:
+            print("Controller: {} --> Up: {}, Down: {}, Left: {}, Right: {}".format(self.controller.get_id(), pad_up,
+                                                                                    pad_down,
+                                                                                    pad_left, pad_right))
+            callback(pad_up, pad_right, pad_down, pad_left)
