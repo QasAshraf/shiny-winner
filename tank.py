bdef065b67eb9f6f1f6295fb76a0161f850fce3d
Steph Cook <3418146+stephegn@users.noreply.github.com>
2017-10-29 01:03:18 +0100

    Maybe rotate

--- tank.py
+++ tank.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 
 # colours
 BLACK    = (   0,   0,   0)
@@ -20,6 +21,7 @@ class Tank(pygame.sprite.Sprite):
         self.boardWidth = boardWidth
         self.boardHeight = boardHeight
         self.joystick = assignedJoystick
+        self.rotation = 0
 
         # Super random colour assignment
         if colour == GREEN:
@@ -87,17 +89,43 @@ class Tank(pygame.sprite.Sprite):
         self.moveDown(pixelsDown)
 
     def moveRight(self, pixels):
-        self.rect.x += pixels
-        if self.rect.x > self.boardWidth - self.width: self.rect.x = self.boardWidth - self.width
+        print(self.rotation)
+        self.rotation += pixels
+        if self.rotation > 360: self.rotation = 0
+        self.image = pygame.transform.rotate(self.image, self.rotation)
+        self.rect = self.image.get_rect(center=self.rect.center)
 
     def moveLeft(self, pixels):
-        self.rect.x -= pixels
-        if self.rect.x < 0: self.rect.x = 0
+        self.rotation -= pixels
+        if self.rotation < 0: self.rotation = 360
+        self.image = pygame.transform.rotate(self.image, self.rotation)
+        self.rect = self.image.get_rect(center=self.rect.center)
 
     def moveUp(self, pixels):
-        self.rect.y -= pixels
-        if self.rect.y < 0: self.rect.y = 0
+        print(self.rotation)
+        # if pixels > 0:
+        #     self.rect.y = pixels * math.sin(self.rotation)
+        #     self.rect.x = math.sqrt(math.pow(pixels, 2) / math.pow(self.rect.y, 2))
+        #     if self.rotation > 180:
+        #         self.rect.y = self.rect.y * -1
+        #         self.rect.x = self.rect.x * -1
+        #     self.checkBoardBoundries()
 
     def moveDown(self, pixels):
-        self.rect.y += pixels
+        print(self.rotation)
+        # self.rect.y += pixels
+
+    def checkBoardBoundries(self):
+        if self.rect.y < 0: self.rect.y = 0
+        if self.rect.x > self.boardWidth - self.width: self.rect.x = self.boardWidth - self.width
         if self.rect.y > self.boardHeight - self.height: self.rect.y = self.boardHeight - self.height
+        if self.rect.x < 0: self.rect.x = 0
+
+    def rot_center(image, angle):
+        """rotate an image while keeping its center and size"""
+        orig_rect = image.get_rect()
+        rot_image = pygame.transform.rotate(image, angle)
+        rot_rect = orig_rect.copy()
+        rot_rect.center = rot_image.get_rect().center
+        rot_image = rot_image.subsurface(rot_rect).copy()
+        return rot_image
